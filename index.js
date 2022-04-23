5062bbfcc69f53898229e8409fcb0ea79cf14bef
George Develops <george.develops19@gmail.com>
2022-04-23 13:21:54 -0400

    Added the cors

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ app.use(morgan('tiny'));
 app.use(express.json());
 require('./middlewares/essentialConfig')();
 
+//CORS 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // Router 
 
 app.use('/api', signup);

fcdd5598d4e8e8535ddd5f1df24be2f96f0500cd
George Develops <george.develops19@gmail.com>
2022-04-25 11:17:25 -0400

    improved add to cart route and delete

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const { accountSchema } = require('./../models/accounts');
-const Product = require('./../models/product');
 const auth = require('./../middlewares/auth');
 const logger = require('./../middlewares/logger');
 const mongoose = require('mongoose');
@@ -23,19 +22,19 @@ router.get('/cart/:customerId', auth, async (req, res)=>{
     }
 });
 
-router.put('/cart/new/:customerId/:productId', auth, async (req, res) => {
+router.put('/cart/new/:customerId', auth, async (req, res) => {
+
     try {
         const customerId = req.params.customerId;
-        const productId = req.params.productId;
 
         const customer = await accountSchema.findById(customerId);
         if (!customer || customer === '') return res.status(404).send("Usuario no encontrado.");
-    
-        const product = await Product.findOne({_id: productId});
-        if (!product || product === '') return res.status(404).send("Producto no encontrado.");
-        const item = product;
+
+        if (!req.body.size || req.body.size === '') return res.status(400).send("Size es obligatorio.");
+
+        const item = { _id: new mongoose.Types.ObjectId(),...req.body};
         
-        await accountSchema.updateOne({_id: customerId}, {$push: { cart: item}});
+        await accountSchema.updateOne({_id: customerId}, {$push: { cart: item }});
         return res.status(200).send("Articulo agregado al carrito con exito.");
     } catch(ex){
         logger.log({
@@ -46,19 +45,13 @@ router.put('/cart/new/:customerId/:productId', auth, async (req, res) => {
     }
 });
 
-router.delete('/cart/delete/:customerId/:productId', auth, async (req, res) => {
+router.delete('/cart/delete/:customerId/:itemId', auth, async (req, res) => {
     try {
         const customerId = req.params.customerId;
-        const productId = req.params.productId;
-
-        const customer = await accountSchema.findById(customerId);
-        if (!customer || customer === '') return res.status(404).send("Usuario no encontrado.");
-
-        const product = await Product.findOne({_id: productId});
-        if (!product || product === '') return res.status(404).send("Producto no encontrado.");
+        const itemId = req.params.itemId;
     
         await accountSchema.updateOne({ _id: mongoose.Types.ObjectId(customerId) }, 
-            { $pull: { 'cart': { _id: mongoose.Types.ObjectId(productId)} }});
+            { $pull: { 'cart': { _id: mongoose.Types.ObjectId(itemId)} }});
     
         return res.status(200).send("Articulo eliminado del carrito con exito.");
     } catch(ex){
